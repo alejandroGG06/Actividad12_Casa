846fed46d9cb86319f779e1df62766953e3cedb1
alejandro <grajalesalejandro068@gmail.com>
2024-03-04 11:52:26 +0100

    jardin actualizado

--- Casa/src/Actividad12_Casa/Jardin.java
+++ Casa/src/Actividad12_Casa/Jardin.java
@@ -3,5 +3,19 @@ package Actividad12_Casa;
 public class Jardin {
 	int metrosCuadrados;
 	int numeroPlantas;
+	public int getMetrosCuadrados() {
+		return metrosCuadrados;
+	}
+	public void setMetrosCuadrados(int metrosCuadrados) {
+		this.metrosCuadrados = metrosCuadrados;
+	}
+	public int getNumeroPlantas() {
+		return numeroPlantas;
+	}
+	public void setNumeroPlantas(int numeroPlantas) {
+		this.numeroPlantas = numeroPlantas;
+	}
+	
+	
 	
 }
