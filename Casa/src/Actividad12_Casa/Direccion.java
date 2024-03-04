3f3f6b94ddef9aa32e09a8602bc238c008f296f1
alejandro <grajalesalejandro068@gmail.com>
2024-03-04 11:50:54 +0100

    Actualizacion main

--- Casa/src/Actividad12_Casa/Direccion.java
+++ Casa/src/Actividad12_Casa/Direccion.java
@@ -7,5 +7,30 @@ public class Direccion {
 	String localidad;
 	int codigoPostal;
 	
+	public tipoVia getTipoVia() {
+		return tipoVia;
+	}
+	public void setTipoVia(tipoVia tipoVia) {
+		this.tipoVia = tipoVia;
+	}
+	public String getNombreVia() {
+		return nombreVia;
+	}
+	public void setNombreVia(String nombreVia) {
+		this.nombreVia = nombreVia;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public int getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(int codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	
 	
 }
